88fb9c192bf5a63a07cbbf7c0d23259ac5939734
Marquis <smcneal89@gmail.com>
2022-10-23 13:30:10 -0400

    updated connection js file and auth.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,12 @@
 //  Bring in connection js
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/sm2-stream';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
