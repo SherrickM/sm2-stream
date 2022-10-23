c52448f9d2ff3a677ad11559f2dc3e4d4c7212ac
Marquis <smcneal89@gmail.com>
2022-10-23 18:47:11 -0400

    updated models for List and Movie

--- server/models/List.js
+++ server/models/List.js
@@ -1,1 +1,27 @@
 // My List , title,  Save, genre, More INfo
+
+const { Schema, model } = require('mongoose');
+
+
+const listSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    moreInfo: {
+      type: String,
+      required: true,
+    },
+
+  },
+  
+);
+
+const List = model('List',listSchema);
+
+module.exports = List;
--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,2 +1,37 @@
-// Title, Description, Poster, Link to Trailer, Year MOvie Came out, Rating, How long the movie is , 
-// Bolean for TV Series
+const {Schema ,model } = require('mongoose');
+
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+    required: true,
+  },
+  trailer: {
+    type: String,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  movieLength: {
+    type: Number,
+    required: true,
+  },
+  tvSeries: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Movie = model('Movie', movieSchema);
+
+module.exports = Movie;
+
